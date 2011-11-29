1fdf494e45eea514a7c4ecb9dc8e86900265ac02
Warren Seymour <wms@fountainhead-solutions.net>
2011-11-29 22:56:39 +0000

    Add failing test

---
+++ tests/cases/extensions/data/source/database/adapter/MySqlTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace li3_geo\tests\cases\extensions\data\source\database\adapter;
+
+class MySqlTest extends \lithium\test\Unit {
+
+    public function testValue() {
+        $this->assertTrue(0);
+    }
+}
+
+?>
